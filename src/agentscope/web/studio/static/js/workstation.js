10300498bd7127a1c0636566b610ff407943cdea
weirui.kwr@alibaba-inc.com <weirui.kwr@alibaba-inc.com>
2024-05-22 12:05:40 +0800

    update

--- src/agentscope/web/studio/static/js/workstation.js
+++ src/agentscope/web/studio/static/js/workstation.js
@@ -1,7 +1,4 @@
-// 获取iframe元素
 var iframe = document.getElementById('workstation-iframe');
-// 构建完整的URL
 var currentUrl = window.location.protocol + '//' + window.location.host + '/workstation';
-// 设置iframe的`src`属性
 iframe.src = currentUrl;
 console.log(iframe.src)
